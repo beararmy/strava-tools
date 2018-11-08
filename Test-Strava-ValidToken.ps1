28ca45c9b203abbf4b4c61aa4aa18fe870c1f1de
Stefan Harrington-Palmer <stef@bear.army>
2018-11-08 21:12:50 +0000

    rename and re-load config every function run

--- Test-Strava-ValidToken.ps1
+++ Test-Strava-ValidToken.ps1
@@ -1,16 +1,17 @@
-# Tests against the token expiry time in json config against now()
+function Test-Strava-ValidToken {
+
 $config = Get-Content -Path .\config.json -Raw | ConvertFrom-Json
 $CurrentTokenExpiresAt = $config.Strava.CurrentTokenExpiresAt
 $now = ([int64](([datetime]::UtcNow) - (get-date "1/1/1970")).TotalSeconds)
-
-function Test-ValidToken {
+    
     if ($CurrentTokenExpiresAt -lt $now) {
-        #Write-Host "Token has expired"
+        Write-Verbose "Token has expired"
         return $false
     }
     else {
-        # Write-Host "Token has"($CurrentTokenExpiresAt - $now)"seconds left"
+        $tokenexpiresin = $CurrentTokenExpiresAt - $now
+        Write-Verbose "Token has $tokenexpiresin seconds left"
         return $true
     }
 }
-#Test-ValidToken
+#Test-Strava-ValidToken
