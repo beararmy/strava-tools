9be1c9d1ab8f5242d146199964502a3afffb43d7
Stefan Harrington-Palmer <stef@bear.army>
2018-11-12 10:56:20 +0000

    bring openweather into align with other test-'s

--- Test-OpenWeather-API.ps1
+++ Test-OpenWeather-API.ps1
@@ -2,21 +2,18 @@ function Test-OpenWeather-API {
 
     $config = Get-Content -Path .\config.json -Raw | ConvertFrom-Json
     $API_Key = $config.weather.API_Key
-    $API_Key = "x"
-    $Weather_API_URI = $config.weather.Weather_API_URI
-    $CityID = $config.weather.Weather_Test_City
-
-    
+    $CityID = $config.weather.Weather_Test_City   
 
     try {
-        $response=(Invoke-RestMethod -Uri ("http://api.openweathermap.org/data/2.5/forecast?id=" + $CityID + "&APPID=" + $api_key + "&units=metric"))
+        $response = (Invoke-RestMethod -Uri ("http://api.openweathermap.org/data/2.5/forecast?id=" + $CityID + "&APPID=" + $api_key + "&units=metric"))
         $response = $response.cod
         if ($response -eq "200") {
-            Write-Verbose "Returned $response, expected $healthy_API_response"
+            Write-Verbose "Returned $response, expected 200"
             return $true
-       }
-     } catch {
-        Write-Verbose "Returned $response, expected $healthy_API_response"
+        }
+    }
+    catch {
+        Write-Verbose "Returned $response, expected 200"
         return $false
     }
     return
