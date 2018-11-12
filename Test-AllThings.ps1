d56a511b9b571dee4e92fa53a49d3f471df2c538
Stefan Harrington-Palmer <stef@bear.army>
2018-11-12 15:15:35 +0000

    Nicely wraps all tests together with crude attempts to resolve

---
+++ Test-AllThings.ps1
@@ -0,0 +1,54 @@
+function Test-AllThings {
+    . "./Test-HASS-API.ps1"
+    . "./Test-OpenWeather-API.ps1"
+    . "./Test-Strava-API.ps1"
+    . "./Test-Strava-ValidToken.ps1"
+    ########################################################        
+    $result = (Test-HASS-API)
+    if ($result -eq $true) {
+        Write-Host "HASS-API " -NoNewline ; Write-Host -ForegroundColor Green "Success"
+    }
+    elseif ($result -eq $false) {
+        Write-Host "HASS-API " -NoNewline ; Write-Host -ForegroundColor Red "Failed"
+    }
+    ########################################################
+    $result = (Test-OpenWeather-API)
+    if ($result -eq $true) {
+        Write-Host "OpenWeather-API " -NoNewline ; Write-Host -ForegroundColor Green "Success"
+    }
+    elseif ($result -eq $false) {
+        Write-Host "OpenWeather-API " -NoNewline ; Write-Host -ForegroundColor Red "Failed"
+    }
+    ########################################################
+    while ($val -ne 3 -and $done -ne 1) {
+        $val++
+        $result = (Test-Strava-ValidToken)
+        if ($result -eq $true) {
+            Write-Host "Strava-ValidToken " -NoNewline ; Write-Host -ForegroundColor Green "Success"
+            $done = '1'
+        }
+        elseif ($result -eq $false) {
+            Write-Host "Strava-ValidToken " -NoNewline ; Write-Host -ForegroundColor Red "Failed"
+            Write-Host "Strava-ValidToken - Attempting to renew token" -ForegroundColor Blue
+            try {
+                . "./Get-Strava-ValidToken.ps1"
+                Write-Host "Strava-ValidToken - re-checking" -ForegroundColor Blue
+            }
+            catch {
+                Write-Host "Strava-ValidToken " -NoNewline ; Write-Host -ForegroundColor Red "FAILED TO RENEW TOKEN"
+                $done = '1'
+            }
+        }
+    }
+    ########################################################
+    $result = (Test-Strava-API)
+    if ($result -eq $true) {        
+        Write-Host "Strava-API " -NoNewline ; Write-Host -ForegroundColor Green "Success"
+    }
+    elseif ($result -eq $false) {
+        Write-Host "Strava-API " -NoNewline ; Write-Host -ForegroundColor Red "Failed"
+        
+    }
+    ########################################################
+    Write-Host ""
+} Test-AllThings
